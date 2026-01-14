77baf6aafe603605a3fdbe1db344f10008d744ed
antoniojoceve <antoniofebrero24@hotmail.com>
2026-01-14 02:25:11 +0100

    feat(orders): add admin approve and reject endpoints

--- src/routes/orders.routes.js
+++ src/routes/orders.routes.js
@@ -2,9 +2,13 @@ const express = require("express");
 const router = express.Router();
 const { getAllOrders } = require("../controllers/orders.controller");
 const { auth, isAdmin } = require("../middlewares/auth.middleware");
+const {getAllOrders, createOrder, approveOrder, rejectOrder} = require("../controllers/orders.controller");
+
 
 router.get("/orders", auth, isAdmin, getAllOrders);
 router.post("/orders", auth, createOrder);
+router.patch("/orders/:id/approve", auth, isAdmin, approveOrder);
+router.patch("/orders/:id/reject", auth, isAdmin, rejectOrder);
 
 
 module.exports = router;
